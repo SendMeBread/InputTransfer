57870f62bb696b4e7d253dcad198335b14811a04
SendMeMyBread <joemczero@gmail.com>
2025-01-26 14:32:32 -0500

    switched to pynput for hosts.

--- Host/Host.py
+++ Host/Host.py
@@ -2,9 +2,27 @@ import subprocess
 import multiprocessing
 import os
 import time
-import keyboard
-import pyautogui
-
+from pynput import keyboard, mouse
+def on_keyDown(key):
+    if key not in inputList:
+        inputList.append(key)
+def on_move(x, y):
+    if "cursorX" not in inputList and "cursorY" not in inputList:
+        inputList.append("cursorX")
+        inputList.append("cursorY")
+def on_click(x, y, button, pressed):
+    if button == button.left:
+        if "left" not in inputList:
+            inputList.append("left")
+    elif button == button.right:
+        if "right" not in inputList:
+            inputList.append("right")
+    elif button == button.middle:
+        if "middle" not in inputList:
+            inputList.append("middle")
+def on_scroll(x, y, dx, dy):
+    if "scroll" not in inputList:
+        inputList.append("scroll")
 address = input("Enter multicast server IP address:\n")
 port = input("Enter multicast server port:\n")
 def run_script(name):
@@ -14,19 +32,12 @@ def run_script(name):
                 filepath = os.path.join(root, file)
                 subprocess.run(["python3", filepath, address, port]) #If on windows, change 'python3' to 'python' #If on windows, change 'python3' to 'python'
 
-curr_time = time.time()
-x1, y1 = pyautogui.position()
 inputList = []
-while curr_time + 30 > time.time():
-    x2, y2 = pyautogui.position()
-    if x2 != x1:
-        inputList.append("CursorX")
-    if y2 != y1:
-        inputList.append("CursorY")
-    event = keyboard.read_event()
-    if event.event_type == keyboard.KEY_DOWN:
-        if event.name not in inputList:
-            inputList.append(event.name.upper())
+with mouse.Listener(on_move=on_move, on_click=on_click, on_scroll=on_scroll) as mListener:
+    mListener.join()
+with keyboard.Listener(on_press=on_keyDown) as kListener:
+    kListener.join()
+time.sleep(20)
 if __name__ == "__main__":
     file_names = inputList
     
