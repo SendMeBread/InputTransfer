757f05bc2bfbd4fd46f52b2144249d3e819e50cc
SendMeMyBread <joemczero@gmail.com>
2025-01-27 17:17:37 -0500

    possibly fixed cursor

--- Clients/Mouse/cursor.py
+++ Clients/Mouse/cursor.py
@@ -10,7 +10,7 @@ class MulticastPingClient(DatagramProtocol):
         while True:
             y2, x2 = mouse.position
             if y2 != y1 or x2 != x1:
-                self.transport.write(bytes(str(y2)+" "+str(x2), encoding='utf-8'), (sys.argv[1], int(sys.argv[2])))
+                self.transport.write(bytes(str(y2),",",str(x2)), encoding='utf-8'), (sys.argv[1], int(sys.argv[2]))
             y1, x1 = y2, x2
     def datagramReceived(self, datagram, address):
         print(f"Datagram {repr(datagram)} received from {repr(address)}")
--- Host/Subhosts/Mouse/cursor.py
+++ Host/Subhosts/Mouse/cursor.py
@@ -8,8 +8,8 @@ class MulticastInputs(DatagramProtocol):
         self.transport.setTTL(5)
         self.transport.joinGroup(sys.argv[1])
     def datagramReceived(self, datagram, address):
-        if b" " in datagram:
-            x, y = tuple(datagram.decode('utf-8').split())
+        if b"," in datagram:
+            x, y = tuple(datagram.decode('utf-8').split(","))
             mouse.move(x, y)
 reactor.listenMulticast(int(sys.argv[2]), MulticastInputs(), listenMultiple=True)
 reactor.run()
