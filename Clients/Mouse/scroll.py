6c278b088424462e99c2ea1a138bf26f23d2154d
SendMeMyBread <joemczero@gmail.com>
2025-01-27 18:10:39 -0500

    Attempt idk

--- Clients/Mouse/scroll.py
+++ Clients/Mouse/scroll.py
@@ -4,17 +4,17 @@ from twisted.internet import reactor
 from twisted.internet.protocol import DatagramProtocol
 class MulticastInputs(DatagramProtocol):
     def on_scroll(self, x, y, dx, dy):
-        self.y_scroll = dy
+        return dy
     def startProtocol(self):
         self.transport.setTTL(5)
         self.transport.joinGroup(sys.argv[1])
         while True:
-            if self.y_scroll != 0:
-                self.transport.write(bytes(str(self.y_scroll), encoding="utf-8"), (sys.argv[1], int(sys.argv[2])))
-                self.y_scroll = 0
+            if self.on_scroll != 0:
+                self.transport.write(bytes(str(self.on_scroll), encoding="utf-8"), (sys.argv[1], int(sys.argv[2])))
+                self.on_scroll = 0
     def datagramReceived(self, datagram, address):
         print(f"Datagram {repr(datagram)} received from {repr(address)}")
-with mouse.Listener(on_move=None, on_click=None, on_scroll=MulticastInputs.on_scroll()) as mListener:
-    mListener.join()
 reactor.listenMulticast(int(sys.argv[2]), MulticastInputs(), listenMultiple=True)
 reactor.run()
+with mouse.Listener(on_move=None, on_click=None, on_scroll=MulticastInputs.on_scroll) as mListener:
+    mListener.join()
