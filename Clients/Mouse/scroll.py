50f9e616d2e7e0d4fe99e84ae9dad911ecaee83c
SendMeMyBread <joemczero@gmail.com>
2025-01-28 15:20:15 -0500

    Forgot a portion

--- Clients/Mouse/scroll.py
+++ Clients/Mouse/scroll.py
@@ -7,9 +7,10 @@ class MulticastInputs(DatagramProtocol):
         self.transport.setTTL(5)
         self.transport.joinGroup(sys.argv[1])
     def on_scroll(self, x, y, dx, dy):
-        y_scroll = dy
-        self.transport.write(bytes(str(y_scroll)+" scrolled", encoding="utf-8"), (sys.argv[1], int(sys.argv[2])))
+        self.y_scroll = dy
+        self.transport.write(bytes(str(self.y_scroll)+" scrolled", encoding="utf-8"), (sys.argv[1], int(sys.argv[2])))
     def datagramReceived(self, datagram, address):
         print(f"Datagram {repr(datagram)} received from {repr(address)}")
+with mouse.Listener(on_scroll=MulticastInputs.on_scroll)
 reactor.listenMulticast(int(sys.argv[2]), MulticastInputs(), listenMultiple=True)
 reactor.run()
