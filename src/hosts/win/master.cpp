d33d06f5245b6dfcd4a145430a073d19acecfdaf
SendMeMyBread <joemczero@gmail.com>
2025-10-14 02:37:53 -0400

    Update windows master.cpp

--- src/hosts/win/master.cpp
+++ src/hosts/win/master.cpp
@@ -38,5 +38,53 @@ void press_B() {
     SendInput(1, &inp, sizeof(INPUT));
 }
 
+void press_C() {
+    INPUT inp;
+
+    inp.type = INPUT_KEYBOARD;
+    inp.ki.wScan = 0;
+    inp.ki.time = 0;
+    inp.ki.dwExtraInfo = 0;
+
+    inp.ki.wVk = VkKeyScan('C');
+    inp.ki.dwFlags = 0;
+    SendInput(1, &inp, sizeof(INPUT));
+
+    inp.ki.dwFlags = KEYEVENTF_KEYUP;
+    SendInput(1, &inp, sizeof(INPUT));
+}
+
+void press_D() {
+    INPUT inp;
+
+    inp.type = INPUT_KEYBOARD;
+    inp.ki.wScan = 0;
+    inp.ki.time = 0;
+    inp.ki.dwExtraInfo = 0;
+
+    inp.ki.wVk = VkKeyScan('D');
+    inp.ki.dwFlags = 0;
+    SendInput(1, &inp, sizeof(INPUT));
+
+    inp.ki.dwFlags = KEYEVENTF_KEYUP;
+    SendInput(1, &inp, sizeof(INPUT));
+}
+
+void press_E() {
+    INPUT inp;
+
+    inp.type = INPUT_KEYBOARD;
+    inp.ki.wScan = 0;
+    inp.ki.time = 0;
+    inp.ki.dwExtraInfo = 0;
+
+    inp.ki.wVk = VkKeyScan('E');
+    inp.ki.dwFlags = 0;
+    SendInput(1, &inp, sizeof(INPUT));
+
+    inp.ki.dwFlags = KEYEVENTF_KEYUP;
+    SendInput(1, &inp, sizeof(INPUT));
+}
+
 int main(int argc, char* argv[]) {
 }
