2db6bb49f9529075c54c9fdc4aac24ba34818a31
SendMeMyBread <joemczero@gmail.com>
2025-10-16 17:29:46 -0400

    Fix up master.cpp (windows) some more.

--- src/hosts/win/master.cpp
+++ src/hosts/win/master.cpp
@@ -14,8 +14,8 @@ std::vector<char> buf(1024);
 
 char keyCharList[] = { '\'', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ';', '=', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', '\\', ']', '`' };
 
-char keyFuncList[] = { '\b', '\t', '\n', '\e', ' ', '<', '>', '^', 'a', 'c', 'd', 'i', 's', 'v', 'w', 'x' };
-int VkList[] = { VK_BACK, VK_TAB, VK_RETURN, VK_SPACE, VK_ESCAPE, VK_LEFT, VK_RIGHT, VK_UP, VK_MENU, VK_CAPITAL, VK_DELETE, VK_INSERT, VK_SHIFT, VK_DOWN, VK_LWIN, VK_CONTROL };
+char keyFuncList[] = { ' ', '<', '>', '^', 'a', 'b', 'c','d', 'e', 'i', 'n', 's', 't', 'v', 'w', 'x' };
+int VkList[] = { VK_SPACE, VK_ESCAPE, VK_LEFT, VK_RIGHT, VK_UP, VK_MENU, VK_BACK, VK_CAPITAL, VK_DELETE, VK_INSERT, VK_RETURN, VK_SHIFT, VK_TAB, VK_DOWN, VK_LWIN, VK_CONTROL };
 
 int find_char_arr_size(char *arr) {
     return sizeof(arr) / sizeof(arr[0]);
@@ -82,35 +82,61 @@ void press_char_key(char mssg) {
     SendInput(1, &inp, sizeof(INPUT));
 }
 
-bool press_functional_key(char mssg) {
-    INPUT inp;
+void press_functional_key(char mssg) {
+    INPUT inp[2];
 
-    inp.type = INPUT_KEYBOARD;
-    inp.ki.wScan = 0;
-    inp.ki.time = 0;
-    inp.ki.dwExtraInfo = 0;
+    inp[0].type, inp[1].type = INPUT_KEYBOARD;
+    inp[0].ki.wScan, inp[1].ki.wScan = 0;
+    inp[0].ki.time, inp[1].ki.time = 0;
+    inp[0].ki.dwExtraInfo, inp[1].ki.dwExtraInfo = 0;
 
     for (const auto& key_pair : keyMap) {
         if (mssg == key_pair.second) {
-            inp.ki.wVk = key_pair.first;
+            inp[0].ki.wVk, inp[2].ki.wVk = key_pair.first;
         }
     }
 
-    inp.ki.dwFlags = 0;
-    SendInput(1, &inp, sizeof(INPUT));
-
-    inp.ki.dwFlags = KEYEVENTF_KEYUP;
-    SendInput(1, &inp, sizeof(INPUT));
-
-    return true;
+    inp[0].ki.dwFlags = 0;
+    inp[1].ki.dwFlags = KEYEVENTF_KEYUP;
+    SendInput(2, inp, sizeof(INPUT));
 }
 
-void move_cursor(std::string mssg) {
-    delimiter = mssg.find(',');
+void move_cursor(std::string coords) {
+    delimiter = coords.find(',');
     if (delimiter != std::string::npos) {
-        x = std::stoi(mssg.substr(0, delimiter));
-        y = std::stoi(mssg.substr(delimiter + 1));
+        x = std::stoi(coords.substr(0, delimiter));
+        y = std::stoi(coords.substr(delimiter + 1));
         SetCursorPos(x, y);
+    }
+}
+
+bool sim_scroll_wheel(std::string factor, char definer) {
+    try {
+        factor.erase(std::remove(factor.begin(), factor.end(), definer), factor.end());
+        mouse_event(MOUSEEVENTF_WHEEL, 0, 0, WHEEL_DELTA * std::stoi(factor), 0);
+        return true;
+    } catch (const std::invalid_argument&) {
+        return false;
+    } catch (const std::out_of_range&) {
+        return false;
+    }
+}
+
+void click_mouse_button(char button) {
+    INPUT Linp[2];
+    INPUT Rinp[2];
+    INPUT Minp[2];
+
+    Linp[0].type, Linp[1].type, Rinp[0].type, Rinp[1].type, Minp[0].type, Minp[1].type = INPUT_MOUSE;
+    Linp[0].mi.dwFlags, Linp[1].mi.dwFlags, Rinp[0].mi.dwFlags, Rinp[1].mi.dwFlags, Minp[0].mi.dwFlags, Minp[1].mi.dwFlags = MOUSEEVENTF_LEFTDOWN, MOUSEEVENTF_LEFTUP, MOUSEEVENTF_RIGHTDOWN, MOUSEEVENTF_RIGHTUP, MOUSEEVENTF_MIDDLEDOWN, MOUSEEVENTF_MIDDLEUP;
+
+    switch (button) {
+        case 'l':
+            SendInput(2, Linp, sizeof(INPUT));
+        case 'r':
+            SendInput(2, Rinp, sizeof(INPUT));
+        case 'm':
+            SendInput(2, Minp, sizeof(INPUT));
     }
 }
 
@@ -123,8 +149,10 @@ void client_handler(SOCKET c_sock){
             press_char_key(mssg_char);
         } else if (mssg_str.contains(",") && mssg_str.length() > 1) {
             move_cursor(mssg_str);
-        } else if (press_functional_key(mssg_char) == false) {
-            std::cerr << "No matching key detected... " << std::endl;
+        } else if (mssg_str.contains("mb") && mssg_str.length() > 2) {
+            click_mouse_button(mssg_str[2]);
+        } else if (!sim_scroll_wheel(mssg_str, mssg_char)) {
+            press_functional_key(mssg_char);
         }
     } while (c_sock > 0);
 }
